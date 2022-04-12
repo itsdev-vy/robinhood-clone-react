e49af40dca2e3fc3d8979793faab3926b7d57054
Vinay <itsvinayadav@gmail.com>
2022-04-12 12:31:01 +0530

    Intialized Robinhood clone project and done with header UI part

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Logo from './robinhood.svg';
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className="header__wrapper">
+            <div className='header__logo'>
+                <img src={Logo} alt='robinhood' width={25} />
+            </div>
+            <div className='header__search'>
+                <div className='header__searchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='Search' type='text' />
+                </div>
+            </div>
+            <div className='header__menuItems'>
+                <a href="/">Free Stocks</a>
+                <a href="/">PortFolio</a>
+                <a href="/">Cash</a>
+                <a href="/">Messages</a>
+                <a href="/">Account</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
